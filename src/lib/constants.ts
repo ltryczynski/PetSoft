e555487671245e99e82d785d4146c87244203619
Łukasz Tryczynski <67525928+ltryczynski@users.noreply.github.com>
2024-12-11 16:33:04 +0100

    Update constants.ts

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,2 +1,2 @@
-export const DEFAULT_PET_PLACEHOLDER = "https://petsoft.ltmedia.pl/pet-placeholder.png";
+export const DEFAULT_PET_PLACEHOLDER = "https://static.ltmedia.pl/assets/img/pet-placeholder.png";
 export const LOGIN_PAGE = "/login";
