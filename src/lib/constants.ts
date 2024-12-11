5de09a411b960bd4e826fdd0ede379d4423d6b4f
krzemu <ltryczynski1@gmail.com>
2024-12-11 15:46:51 +0100

    Changed placeholder url

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,2 +1,2 @@
-export const DEFAULT_PET_PLACEHOLDER = "https://bytegrad.com/course-assets/react-nextjs/pet-placeholder.png";
+export const DEFAULT_PET_PLACEHOLDER = "https://petsoft.ltmedia.pl/pet-placeholder.png";
 export const LOGIN_PAGE = "/login";
