93f34d725906380cbc3a0a4c53a170557942ec51
Łukasz Tryczynski <67525928+ltryczynski@users.noreply.github.com>
2024-12-11 16:35:50 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,8 @@ const nextConfig = {
         remotePatterns: [
             {
                 protocol: 'https',
-                hostname: 'bytegrad.com',
+                hostname: 'static.ltmedia.pl',
+                pathname: '/assets/img/**'
             },
             {
                 protocol: 'https',
